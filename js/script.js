d5ebeb908d8bed2261e558220935a10775d6d995
Het Patel <hp428@njit.edu>
2020-07-28 12:47:33 -0500

    Update in css and js file for animation

--- js/script.js
+++ js/script.js
@@ -9,11 +9,19 @@ const nav = document.querySelector(".nav"),
 for(let i=0; i<totalNavList; i++){
 	const a = navList[i].querySelector("a");
 	 a.addEventListener("click", function(){
-	 	for(let j=0; j<totalNavList; j++){
-	 		navList[j].querySelector("a").classList.remove("active");
-	 	}
-	 	this.classList.add("active");
+	 		
+	 	for(let i=0; i<totalSection; i++){
+			allSection[i].classList.remove("back-section");
+		}
 
+	 	for(let j=0; j<totalNavList; j++){
+		 	if(navList[j].querySelector("a").classList.contains("active")){
+		 		allSection[j].classList.add("back-section");
+		 	}
+		 	navList[j].querySelector("a").classList.remove("active");
+	 	}
+
+	 	this.classList.add("active");
 	 	showSection(this);
 	 })
 }
