c199a888c55e90dc444060afed13454f9b09e6d4
Het Patel <hp428@njit.edu>
2020-07-29 08:18:54 -0500

    Updated the js file for page link

--- js/script.js
+++ js/script.js
@@ -18,13 +18,11 @@ for(let i=0; i<totalNavList; i++){
 	const a = navList[i].querySelector("a");
 	 a.addEventListener("click", function(){
 	 		
-	 	for(let i=0; i<totalSection; i++){
-			allSection[i].classList.remove("back-section");
-		}
+	 		removeBackSectionClass();
 
 	 	for(let j=0; j<totalNavList; j++){
 		 	if(navList[j].querySelector("a").classList.contains("active")){
-		 		allSection[j].classList.add("back-section");
+		 		addBackSectionClass(j)
 		 	}
 		 	navList[j].querySelector("a").classList.remove("active");
 	 	}
@@ -34,6 +32,14 @@ for(let i=0; i<totalNavList; i++){
 	 })
 }
 
+function removeBackSectionClass(){
+	for(let i=0; i<totalSection; i++){
+		allSection[i].classList.remove("back-section");
+	}
+}
+function addBackSectionClass(num){
+	allSection[num].classList.add("back-section");
+}
 function showSection(element){
 	for(let i=0; i<totalSection; i++){
 		allSection[i].classList.remove("active");
@@ -41,6 +47,23 @@ function showSection(element){
 	const target = element.getAttribute("href").split("#")[1];  
 	document.querySelector("#"+target).classList.add("active")
 }
+
+function updateNav(element){
+	for(let i=0; i<totalNavList; i++){
+		navList[i].querySelector("a").classList.remove("active");
+		const target = element.getAttribute("href").split("#")[1];
+		if(target == navList[i].querySelector("a").getAttribute("href").split("#")[1]){
+			navList[i].querySelector("a").classList.add("active");
+		}
+	}
+}
+document.querySelector(".hire-me").addEventListener("click",function(){
+	const sectionIndex = this.getAttribute("data-section-index");
+	showSection(this);
+	updateNav(this);
+	removeBackSectionClass();
+	addBackSectionClass(sectionIndex);
+})
 
 // const navTogglerBtn = document.querySelector(".nav-toggler"),
 // 	  aside = document.querySelector(".aside");
